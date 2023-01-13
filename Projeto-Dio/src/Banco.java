1b5ce2c3d55f57d035fe92146ec1a681fbb6825e
Annajucaval <annajuliacaval@gmail.com>
2023-01-13 15:52:04 -0300

    Desafio Banco

---
+++ Projeto-Dio/src/Banco.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+ 
+class Main { 
+ 
+    public static void main(String[] args) { 
+ 
+    String[] nomesFila = new String[3]; 
+    Scanner nome = new Scanner(System.in); 
+
+    for (int i = 0; i < nomesFila.length; i++) {
+        nomesFila[i] = nome.next();
+
+        System.out.format("%s - esta na posicao: %d%n",nomesFila[i],(i + 1) );
+  } 
+}
+}
--- Projeto-Dio/src/Turno.java
+++ Projeto-Dio/src/Turno.java
@@ -4,8 +4,6 @@ class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-System.out.println("Em que turno você estuda?");
-
 
         char turno = leitor.next().toUpperCase().charAt(0);
         
