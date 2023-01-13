79fbb3ca599cab131127db0469301676feaf9e34
Annajucaval <annajuliacaval@gmail.com>
2023-01-13 15:39:03 -0300

    Desafio Turno

--- Projeto-Dio/src/MediaIdade.java
+++ Projeto-Dio/src/MediaIdade.java
@@ -1,35 +1,26 @@
 import java.util.Scanner;
 
- class IdadeTurma {
-    public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        
-
-  int i1 = 7;
-  int i2 = 14;
-  int i3 = 22;
-  int soma = 0;
-int count = 0;
-  do {
-System.out.printf("Idade da Turma");
-i1 = leitor.nextInt();
-i2 = leitor.nextInt();
-i3 = leitor.nextInt();
-
-soma = soma + i1 + i2 + i3;
-
-
-
-  }while(count < 3);
-
-
- 
+class Main {
+public static void main(String[] args) {
+     Scanner leitor = new Scanner(System.in);
        
-  if (soma/3<=26) {System.out.println("Joven!");
-        
-}else if(soma>=60) {System.out.println("Idosa!");
-}
+     int idade1 = leitor.nextInt();
+     int idade2 = leitor.nextInt();
+     int idade3 = leitor.nextInt();
 
+      double  m = (idade1 + idade2 + idade3)/3;
+
+        
+        if (m<=26) {System.out.printf("Jovem!");
+          
+        } 
+         else if (m<=60) {System.out.printf("Adulta!");
+          
+        }
+        else  {System.out.printf("Idosa!");
+      
+      }
     }
-}
+  }
+
     
