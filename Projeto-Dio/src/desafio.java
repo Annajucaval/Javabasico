9ec004697c778d3f12b0953cb603f5c352651913
Annajucaval <annajuliacaval@gmail.com>
2023-01-25 10:16:06 -0300

    desafio digítos

---
+++ Projeto-Dio/src/desafio.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+ class Main {
+	public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+
+    int numero = scan.nextInt();
+
+    String numeroEmString = String.valueOf(numero);
+    String[] split = numeroEmString.split("");
+
+    
+    int[] ints = Arrays.stream(split).mapToInt(Integer::parseInt).toArray();
+      
+        int produto = 1;
+        int soma = 0;
+        int resultado = 0;
+        
+        for (int number : ints){
+            produto*= number;
+            soma += number;
+        }
+        System.out.println(produto - soma);
+        
+        
+    }
+}
